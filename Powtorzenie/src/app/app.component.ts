b310befdbe9b8d90a9cbc88713bd7aa25b8ed69e
r3czus <76210775+r3czus@users.noreply.github.com>
2022-09-06 18:09:13 +0200

    dalszy kod
    giga chat

--- Powtorzenie/src/app/app.component.ts
+++ Powtorzenie/src/app/app.component.ts
@@ -7,17 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Powtorzenie';
-  kursyProgramowanie=["Js",'Python',"PHP","Node.js"];
-  urlzdj1:string="assets/Images/zdj2.png";
-  urlzdj2:string="assets/Images/zdj4.jpg";
-  urlzdj3:string="assets/Images/zdj5.jpg";
-  aktywne:boolean=true;
-  licznik:number=1;
-  brak:string="assets/Images/noPicture.jpg";
-  zwieksz():void{
+  kursyProgramowanie = ["Js", 'Python', "PHP", "Node.js"];
+  urlzdj1: string = "assets/Images/zdj2.png";
+  urlzdj2: string = "assets/Images/zdj4.jpg";
+  urlzdj3: string = "assets/Images/zdj5.jpg";
+  aktywne: boolean = true;
+  licznik: number = 1;
+  brak: string = "assets/Images/noPicture.jpg";
+  wybrany: number = 0;
+  kolory: string[] = ["red", "blue", "green", "purple"]
+
+
+  zwieksz(): void {
     this.licznik++;
   }
-  Zobacz():void{
-     this.aktywne=!this.aktywne
+  Zobacz(): void {
+    this.aktywne = !this.aktywne
+  }
+  Formatuj(): void {
+    this.wybrany++;
+    if (this.wybrany > this.kolory.length - 1) {
+      this.wybrany = 0;
+    }
   }
 }
